34df1bb2a12efdac253e81ebec36602f572f033e
bxppo <abhit999@gmail.com>
2022-08-20 22:41:11 +0530

    resources

--- src/components/Resources/index.tsx
+++ src/components/Resources/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
+import "../../styles/dash.css";
 const Resources = () =>
 {
   return (
-    <>
 
       <div className="container">
-      RESOURCETAB
+      <div className="reshead">
+          <h2>Resources</h2>
+          <button className="dashbutton">
+          <a href="#" >Go to Docs</a>
+          </button>
+          <div className="resvideo"></div>
+              <h4>Videos</h4>
       </div>
-    </>
+      </div>
   );
 };
 
