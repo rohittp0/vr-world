ca419894bd783e737033ce7a78651e4085664457
bxppo <abhit999@gmail.com>
2022-08-21 20:24:50 +0530

    resources

--- src/components/Resources/index.tsx
+++ src/components/Resources/index.tsx
@@ -6,6 +6,7 @@ import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Typography from "@mui/material/Typography";
 import YouTubeIcon from "@mui/icons-material/YouTube";
+import TextField from "@mui/material/TextField";
 const Resources = () =>
 {
   return (
@@ -14,7 +15,17 @@ const Resources = () =>
       <div className="reshead">
           <h2>Resources</h2>
           <div className="prfdivider"/>
-          <input type="text"  className="resbar" placeholder="Q  Search"/>
+    <TextField label="Search"
+               className="inputRounded" placeholder="Search" variant="outlined"
+               inputProps={{
+                   style: {
+                       width: "80px",
+                       borderColor: "#ff1e56",
+
+                   }
+               }}
+    />
+          {/*<input type="textarea"  className="resbar" placeholder="Q  Search"/>*/}
       </div>
           <button className="dashbutton">
           <a href="#" >Go to Docs</a>
@@ -43,8 +54,8 @@ const Resources = () =>
                       </CardContent>
                       <CardActions sx={{backgroundColor:"#f5f5f5"}}>
                           <a href="https://youtu.be/dQw4w9WgXcQ" className="watchbutton">Watch
-                      </a>
                           <YouTubeIcon sx={{ color: "red" }} />
+                          </a>
                       </CardActions>
                   </Card>
                   <Card sx={{ maxWidth: 345, borderRadius:"15px"}}>
