4816200b73000631a0911782673c0d721570587c
bxppo <abhit999@gmail.com>
2022-08-24 12:51:45 +0530

    resources

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -3,9 +3,6 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import {Avatar, Typography} from "@mui/material";
 import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import "../../styles/edit.css"
 
 const Register = () =>
@@ -16,9 +13,9 @@ const Register = () =>
         <div className="editCont">
               <h4>Complete Profile</h4>
                      <Avatar sx={{ width: 140, height: 140, backgroundColor:"red", borderRadius:"10px" }}>H</Avatar>
-                      <div className="editComponents">
-                         <h6>Name</h6>
-                     <TextField id="standard-basic" variant="standard" required  />
+                      <form className="editComponents">
+                         <h6>Name<sup>*</sup></h6>
+                          <TextField required id="Name" variant="standard" />
                          <h6>Bio</h6>
                     <TextField id="standard-basic"variant="standard" />
                         <input
@@ -36,7 +33,7 @@ const Register = () =>
                     <TextField id="standard-basic"  variant="standard" />
                          <h6>LinkedIn Link</h6>
                     <TextField id="standard-basic"  variant="standard" />
-                     </div>
+                     </form>
               <input type="button" className="edtbutton" value="Save"/>
           </div>
           </>
